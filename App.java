4569549ec5ecb94b6c543dbf6a8f545e89b59ef4
kishore0606 <110541737+kishore0606@users.noreply.github.com>
2022-08-05 22:04:09 +0530

    Patterns

--- App.java
+++ App.java
@@ -1,95 +1,163 @@
-import java.util.Scanner;
-
-public class App {
-    static int tar=Integer.MAX_VALUE;
-    public static void main(String[] args) throws Exception {
-        Scanner sc=new Scanner(System.in);
-        while(true)
+public class App 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        int length=11;
+        for(int i=0;i<length;i++)
         {
-            System.out.println("Menu :");
-            System.out.println("---------------------------------------------");
-            System.out.println("1. Start game / Reset game");
-            System.out.println("2. Try again");
-            System.out.println("3. Exit game");
-            System.out.println(" ");
-            System.out.println("Enter the option :");
-            int opt=sc.nextInt();
-            switch(opt)
+            for(int j=0;j<length;j++)// I
             {
-                case 1:     
-                    System.out.println("Enter the target value :");
-                    int target=sc.nextInt();
-                    tar=target;
-                    System.out.println("How many players want to play the game ?");
-                    int players_count=sc.nextInt();
-                    int[] players=new int[players_count];
-                    String outp="";
-                    int count_players=0;
-                    for(int i=0;i<players_count;i++)
+                if(i==0 || i==length-1)
+                {
+                    System.out.print("*");
+                }
+                else if(i>0 && i<length-1)
+                {
+                    if(j==length/2)
                     {
-                        System.out.println("Enter player "+(i+1)+" guess :");
-                        players[i]=sc.nextInt();
-                        if(players[i]==target)
-                        {
-                            outp=outp+" player"+(i+1)+" ,";
-                            count_players++;
-                        }
+                        System.out.print("*");
                     }
-                    if(count_players==0)
+                    else
                     {
-                        System.out.println("No players guessed the target right.");
+                        System.out.print(" ");
                     }
-                    else if(count_players>0 && count_players<players_count)
-                    {
-                        System.out.println("Game Tied between "+outp.substring(0,outp.length()));
-                    }
-                    else if(count_players==players_count)
-                    {
-                        System.out.println("Game Tied among all players.");
-                    }
-                    break;
-                case 2:
-                    int target1=tar;
-                    if(tar==Integer.MAX_VALUE)
-                    {
-                        System.out.println("No previouos games found, Kindly enter new value.");
-                        System.out.println("Enter the target value :");
-                        target1=sc.nextInt();
-                    }
-                    System.out.println("How many players want to play the game :");
-                    int players_count1=sc.nextInt();
-                    int[] players1=new int[players_count1];
-                    String outp1="";
-                    int count_players1=0;
-                    for(int i=0;i<players_count1;i++)
-                    {
-                        System.out.println("Enter player "+(i+1)+" guess :");
-                        players1[i]=sc.nextInt();
-                        if(players1[i]==target1)
-                        {
-                            outp1=outp1+" player"+(i+1)+" ,";
-                            count_players1++;
-                        }
-                    }
-                    if(count_players1==0)
-                    {
-                        System.out.println("No players guessed the target right :");
-                    }
-                    else if(count_players1>0 && count_players1<players_count1)
-                    {
-                        System.out.println("Game Tied between "+outp1.substring(0,outp1.length()-1));
-                    }
-                    else if(count_players1==players_count1)
-                    {
-                        System.out.println("Game Tied among all players.");
-                    }
-                    break;
-                case 3:
-                    System.exit(0);
-                default:
-                    System.out.println("Enter valid option.");
-                    break;     
+                }
             }
+            System.out.print("   ");
+            for(int j=0;j<length;j++)// N
+            {
+                if(j==0 || j==length-1 || i==j)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("   ");
+            for(int j=0;j<length;j++)// E
+            {
+                if(i==0 || i==length/2 || i==length-1 || j==0) 
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("   ");
+            for(int j=0;j<length;j++)// U
+            {
+                if(j==0 && i<length-2 || j==length-1 && i<length-2 || (i>length-2 && j<length-2)&&j>=2 || i==length-2 && j==1 || i==length-2 && j==length-2)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("   ");
+            for(int j=0;j<length;j++)// R
+            {
+                if(((i==0 || i==(length/2)) && j<length-4 ) || ((i<(length/2)-1&& i>1)  && j==length-2) || j==0 || i-j+1==(length/2) || (i==1 && j==length-3) || (i==(length/2)-1 && j==length-3))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("   ");
+            for(int j=0;j<length;j++)// O
+            {
+                if( (i>1 && i<length-2) && (j==0 || j==length-1) || (i==0 || i==length-1) && (j>1 && j<length-2) || (i==1||i==length-2)&&(j==1 || j==length-2)   )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("   ");
+            for(int j=0;j<length;j++)// N
+            {
+                if(j==0 || j==length-1 || i==j)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("   ");
+            System.out.println(" ");
         }
+        // numbers
+
+        int count=5;
+        int value=1;
+        for(int i=0;i<count;i++)
+        {
+            for(int j=0;j<count;j++)
+            {
+                System.out.print(value);
+            }
+            System.out.println();
+            value++;
+        }
+
+        // patterns
+        int new_length=12;
+        for(int i=0;i<new_length;i++)
+        {
+            for(int j=0;j<new_length;j++)
+            {
+                if(j==0 || j==new_length-1 || i>new_length/2&&j==new_length ||j-i>=(new_length-1)/2||i+j<6)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("   ");
+
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<(new_length-i-1)*2;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+
+            System.out.print("   ");
+
+            for(int j=1;j<=new_length;j++)
+            {
+                if(i==0 || j==1  || i==new_length-1 || i+j<(new_length/2)+2 || i-j>(new_length/2)-2)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println(" ");
+        
+        }
+        
     }
 }
+
